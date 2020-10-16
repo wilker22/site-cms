d6003abeae5daf760ff6a3588bab897b641269ca
Wilker <wilker_a@hotmail.com>
2020-10-16 20:55:10 -0300

    estrutura de banco de dados, models, factories e migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['category_id','name', 'description', 'exclusive'];
+    protected $dates = ['deleted_at'];
+
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
+
+
+}
